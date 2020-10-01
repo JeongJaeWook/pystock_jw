5f2bd27fee33e94813e78a36d76ccc053213654d
JeongJaeWook <jaewook111@naver.com>
2020-10-01 17:38:38 +0900

    20201001 17:40 스파르타 파이썬 코딩 수업1

---
+++ spartacoding/pythonpackage_test.py
@@ -0,0 +1,5 @@
+import dload
+
+dload.save("https://spartacodingclub.kr/static/css/images/ogimage.png", 'sparta.png')
+dload.save("https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F081%2F2018%2F05%2F23%2F0002917427_001_20180523181610317.jpg&type=sc960_832",'dog.png')
+
